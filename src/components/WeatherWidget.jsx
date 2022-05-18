abea677361723936b90d7d8be51b381ab5796ffa
Maksim Shelest <shelest.ks@gmail.com>
2022-05-18 16:35:47 +0300

    input

--- src/components/WeatherWidget.jsx
+++ src/components/WeatherWidget.jsx
@@ -18,38 +18,40 @@ const WeatherWidget = () => {
 	}
 
 	return (
-				<div className='widget'>
-					<input
-						type="text"
-						value={location}
-						onKeyDown={searchLocation}
-						onChange={e => setLocation(e.target.value)}
-						placeholder="Введите город"
-					/>
-					<div className='widget__top'>
-						<div className="widget__location">{data.name}</div>
-						<div className="widget__temp">{data.main ? Math.round(data.main.temp) : ''} °C</div>
-						<div className="widget__icon">
-							{data.weather ? <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="" /> : ""}
-						</div>
-						<div className="widget__description">{data.weather ? data.weather[0].description : ""}</div>
-					</div>
-					<div className='widget__bottom'>
-						<div className="widget__feels">
-							<p>Ощущается как</p>
-							<p>{data.main ? Math.round(data.main.feels_like) : ""} °C</p>
-						</div>
-						<div className="widget__humidity">
-							<p>Влажность</p>
-							<p>{data.main ? data.main.humidity : ""}%</p>
-						</div>
-						<div className="windget__wind">
-							<p>Ветер</p>
-							<p>{data.wind ? data.wind.speed : ""}м/c</p>
-						</div>
-					</div>
-					
+		<div className='widget'>
+			<div className="widget__search">
+				<input
+					type="text"
+					value={location}
+					onKeyDown={searchLocation}
+					onChange={e => setLocation(e.target.value)}
+					placeholder="Введите город"
+				/>
+			</div>
+			<div className='widget__top'>
+				<div className="widget__location">{data.name}</div>
+				<div className="widget__temp">{data.main ? Math.round(data.main.temp) : ''} °C</div>
+				<div className="widget__icon">
+					{data.weather ? <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="" /> : ""}
 				</div>
+				<div className="widget__description">{data.weather ? data.weather[0].description : ""}</div>
+			</div>
+			<div className='widget__bottom'>
+				<div className="widget__feels">
+					<p>Ощущается как</p>
+					<p>{data.main ? Math.round(data.main.feels_like) : ""} °C</p>
+				</div>
+				<div className="widget__humidity">
+					<p>Влажность</p>
+					<p>{data.main ? data.main.humidity : ""} %</p>
+				</div>
+				<div className="windget__wind">
+					<p>Ветер</p>
+					<p>{data.wind ? data.wind.speed : ""} м/c</p>
+				</div>
+			</div>
+
+		</div>
 
 	);
 };
